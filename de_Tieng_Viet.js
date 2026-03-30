4459181411c8c5071987cd629ff1a8067bf7b430
Phan Văn Tuyến <pvtuyen@moet.edu.vn>
2026-03-30 16:17:46 +0700

    Update de_Tieng_Viet.js

--- de_Tieng_Viet.js
+++ de_Tieng_Viet.js
@@ -7,7 +7,7 @@ const EXAM_STRUCTURE = [
     youtube_url:"https://www.youtube.com/embed//iEFW8PWQgRg",
     options:["mùa xuân", "mùa hè", "mùa thu", "mùa đông"],
     answer:"mùa xuân",
-    max_score: 0
+    max_score: 1
   },
       
   {
@@ -34,19 +34,19 @@ const EXAM_STRUCTURE = [
       "Trầm mặc, thành kính"
     ],
     answer: "Nườm nượp, xúng xính, cởi mở",
-    max_score: 2
+    max_score: 1
   },
   {
     id: "t3",
     type: "ai",
     question: "Câu 3: Em hãy giải thích ý nghĩa của hai câu thơ: 'Đất nước mình thanh lịch / Nên núi rừng cũng thơ'. Qua đó, em cảm nhận được tình cảm gì của tác giả dành cho quê hương?",
-    rubric: "- Giải thích được từ 'thanh lịch' gắn với vẻ đẹp con người và đất nước (1.5đ)\n- Nêu được mối quan hệ giữa con người và thiên nhiên (núi rừng cũng trở nên có hồn, nên thơ) (1.5đ)\n- Thể hiện được niềm tự hào, tình yêu quê hương đất nước sâu sắc của tác giả (2đ).",
-    max_score: 5
+    rubric: "- Giải thích được từ 'thanh lịch' gắn với vẻ đẹp con người và đất nước (0.5đ)\n- Nêu được mối quan hệ giữa con người và thiên nhiên (núi rừng cũng trở nên có hồn, nên thơ) (0.5đ)\n- Thể hiện được niềm tự hào, tình yêu quê hương đất nước sâu sắc của tác giả (1đ).",
+    max_score: 2
   },
   {
     id: "t4",
     type: "multimedia",
     question: "Câu 4: Em hãy chép lại 4 khổ thơ đầu của bài 'Đi hội chùa Hương' vào vở sao cho đúng thể thơ 5 chữ, trình bày sạch đẹp và chụp ảnh gửi lên hệ thống.",
-    max_score: 3
+    max_score: 4
   }
 ];
